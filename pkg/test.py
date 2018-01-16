5ba7ba09e0483ee78d39e1c649369abbc8d4bc39
于金玉 <yujinyu@hust.edu.cn>
2018-01-16 10:44:46 +0800

    ...

--- pkg/test.py
+++ pkg/test.py
@@ -2,24 +2,23 @@ import os
 import time
 import docker
 from random import Random
-from pkg.lockstat import start as lockstat_start, stop as lockstat_stop, get as get_lockstat
 from pkg.cpu import get_num_of_cpus
 
 image = "test:cpu-bound"
-cpu_flag = ["0","1"]
 m_limit = "4g"
 
-def random_string(llen=6):
+def random_string(le=6):
     string = ""
     chars = "AaBbCDdEeFfGgHhJKkLMmNnPpQqRrSTtUuVvWwXxYy3456789"
     length = len(chars) - 1
     random = Random()
-    for l in range(llen):
+    for l in range(le):
         string += chars[random.randint(0, length)]
     return string
 
 
 def build_image(path2dockerfile, client=docker.from_env()):
+    client.login(username="yy",password="12345678",registry="192.168.3.51:5000/admin")
     try:
         client.images.build(path=path2dockerfile, tag=image)
         print("Build image Successfully!")
@@ -78,39 +77,40 @@ def rm_cntrs(clt):
 
 
 class Test:
-    # rw_mode
-    # fio: write , read
-    # sysbench: seqwr, seqrd
-    # iozone:  -i  x
-    #  0=write/rewrite, 1=read/re-read, 2=random-read/write,  3=Read-backwards
-    #  4=Re-write-record, 5=stride-read,  6=fwrite/re-fwrite,  7=fread/Re-fread
-    #  8=random mix, 9=pwrite/Re-pwrite, 10=pread/Re-pread
-    # 11=pwritev/Re-pwritev, 12=preadv/Re-preadv
-
     def __init__(self, storage_device, fs_type, mount_point, result_dir, scale_test, direct_io=True):
-        self._client = docker.from_env()
         self._device = storage_device
         self._fs_type = fs_type
         self._mnt_point = mount_point
         self._result_directory = result_dir
         self._scale_test = scale_test
-        self._io_flag = direct_io
         self._max_num = get_num_of_cpus() + 1
-        self._saved_image = "%s.tar" % image.replace(":", "-")
-        self._res_dirs = []
 
     def _pre_work(self):
-        docker_svc_stop()
-        mkfs_mnt(self._device, self._fs_type, "/var/lib/docker")
-        os.system("systemctl restart docker")
-        print(os.getcwd())
         build_image(os.path.join(os.getcwd(),"image_built"))
-
-    # def _crt_run(self, rng, cmd, volume):
-
-
-    # def _ex_test(self, cmd, vol, rng, scale_test=True):
 
 
     def start(self):
         self._pre_work()
+        result_directory = os.path.join(self._result_directory, "cpu")
+        volume = {result_directory: self._mnt_point}
+        client = docker.from_env()
+        client.containers.run(image=image,
+                              command="./run.sh 3 3cpu-sets",
+                              detach=True,volumes=volume,
+                              cpuset_cpus="0-2",
+                              mem_limit=m_limit,
+                              name=random_string(8),
+                              working_dir="/")
+        while whether_wait(client):
+            time.sleep(45)
+
+        client.containers.run(image=image,
+                              command="./run.sh 3 3cpu-shares",
+                              detach=True, volumes=volume,
+                              cpu_shares=3,
+                              mem_limit=m_limit,
+                              name=random_string(8),
+                              working_dir="/")
+
+        while whether_wait(client):
+            time.sleep(45)
--- run.py
+++ run.py
@@ -8,7 +8,6 @@ result_dir = "/home/result"
 if __name__ == "__main__":
     # prepare(os.path.join(os.getcwd(), "image_built"))
     fs = "ext4"
-    print(fs + 64 * "*")
     test = Test(device, fs, "/mnt", result_dir, scale_test=True, direct_io=False)
     test.start()
     # res = Analysis(result_dir)
